7f857dc6f1a399d11cede5733f9028a661e52549
Herman Sletmoen <hermansletmoen@gmail.com>
2023-01-29 12:22:10 +0100

    Sleep 1s on USB connect

--- rm_sync_on_connect_trigger.sh
+++ rm_sync_on_connect_trigger.sh
@@ -10,5 +10,5 @@ dir=$(dirname $0) # e.g. /home/hermasl/Remarkable/rmirro
 # as the right user and in the right directory,
 # requiring the user to confirm its actions
 # TODO: instead of hard-coding the Gnome console (kgx), open the user's default terminal emulator?
-sudo --user=$user kgx -- sh -c "cd $dir; echo \"User: \$USER\"; echo \"Directory: \$PWD\"; ./rmirro.py" &
+sudo --user=$user kgx -- sh -c "cd $dir; echo \"Auto-synchronising reMarkable that was just connected with USB\"; echo \"User: \$USER\"; echo \"Directory: \$PWD\"; sleep 1; ./rmirro.py" &
 #sudo --login --user=hermasl DISPLAY=":0" DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$UID/bus" kgx -- sh -c "cd $script_dir; ./rmirro.py --dry-run" & # \; ./rmirro.py --dry-run & # these extra environment variables don't seem to be needed
